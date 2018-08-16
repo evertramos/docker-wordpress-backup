02cc91b27f456722bf5a3e61c63854dbca7d9423
Evert - Unaids - am01 <evert.ramos@gmail.com>
2018-08-16 21:19:46 +0000

    add auto settings on .env file

--- set-crontab.sh
+++ set-crontab.sh
@@ -1,12 +1,36 @@
 #!/usr/bin/env bash
 
+source .env
+
+if [ $? -ne 0 ]; then
+    echo 
+    echo ">>> ---------------------------------------------------------"
+    echo ">>> "
+    echo ">>> You must symlink your '.env' file with your settings from"
+    echo ">>> your docker-wordpress-letsencrypt folder and set contrab"
+    echo ">>> rule variable at '\$BACKUP_CRONTAB_RULE'"
+    echo ">>> "
+    echo ">>> ---------------------------------------------------------"
+    echo 
+    exit 1
+fi
+
+if [ -z ${BACKUP_CRONTAB_RULE+X} ]; then
+    echo 
+    echo ">>> ---------------------------------------------------------"
+    echo ">>> "
+    echo ">>> The variable '\$BACKUP_CRONTAB_RULE' was not set in your"
+    echo ">>> '.env' file. Please update this option and try again."
+    echo ">>> "
+    echo ">>> ---------------------------------------------------------"
+    echo 
+    exit 1
+fi
+
 CRONTAB_FILE=current_crontab
 CRONTAB_SCRIPT="$(dirname "$(readlink -f "$0")")""/run-backup.sh"
 CRONTAB_SIGNATURE="CRONTAB_RULE_DOCKER_WORDPRESS_BACKUP"
-
-
-#CRONTAB_RULE="00 03 * * 1-5 $CRONTAB_SCRIPT"
-CRONTAB_RULE="00 21 * * 1-5 "$CRONTAB_SCRIPT" # "$CRONTAB_SIGNATURE
+CRONTAB_RULE=$BACKUP_CRONTAB_RULE" "$CRONTAB_SCRIPT" # "$CRONTAB_SIGNATURE
 
 # Write your current crontab to a file
 crontab -l > $CRONTAB_FILE
@@ -21,6 +45,11 @@ echo "$CRONTAB_RULE" >> $CRONTAB_FILE
 crontab $CRONTAB_FILE
 
 rm $CRONTAB_FILE
+
+echo
+echo "Crontab updated with this settings:"
+echo "$CRONTAB_RULE"
+echo
 
 exit 0
 
